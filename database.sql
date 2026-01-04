3aa1d9a69663e71403ad0a69b8d722f0502078c3
Austin-T-Rock <austinrock03@gmail.com>
2026-01-03 21:59:33 -0500

    Update at 1/3/2026, 9:59:33 PM

--- database.sql
+++ database.sql
@@ -2,7 +2,7 @@
 -- PostgreSQL database dump
 --
 
-\restrict U83EFm5brPxPatmOmdJsSQIqdTHzLhsKVi2hYQsiVIXdxObzUns8y9U6WSHrHvo
+\restrict 9BgNjv7PgUnzN7ssUyOTYdE1dZDxr4fWliOoi0XhgE8V4tb6CAqNK5Vg5exxdr4
 
 -- Dumped from database version 18.1
 -- Dumped by pg_dump version 18.1
@@ -26896,6 +26896,7 @@ COPY public.people (id, name, birth_date, death_date) FROM stdin;
 7891	Willa Fitzgerald	1991-01-17	\N
 509	Javier Bardem	1969-03-01	\N
 2292	Frederick Kerr	1858-10-11	1933-05-03
+7892	Charlie Chaplin	1889-04-16	1977-12-25
 \.
 
 
@@ -31241,5 +31242,5 @@ ALTER TABLE ONLY public.seasons_writers
 -- PostgreSQL database dump complete
 --
 
-\unrestrict U83EFm5brPxPatmOmdJsSQIqdTHzLhsKVi2hYQsiVIXdxObzUns8y9U6WSHrHvo
+\unrestrict 9BgNjv7PgUnzN7ssUyOTYdE1dZDxr4fWliOoi0XhgE8V4tb6CAqNK5Vg5exxdr4
 
