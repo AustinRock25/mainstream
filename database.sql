e66d3f3b494e1fea4faf5a3d75a91877a8c35c2f
Austin-T-Rock <austinrock03@gmail.com>
2026-01-13 21:59:46 -0500

    Update at 1/13/2026, 9:59:46 PM

--- database.sql
+++ database.sql
@@ -2,7 +2,7 @@
 -- PostgreSQL database dump
 --
 
-\restrict RajgSL1e8uNAvifaKkgHVrzT8BpNdXQKEA1imKWGYLcbLdKA01rjOtv6F51BSC8
+\restrict J4thi70qUQOSTU1ldpS0pogcGaGnb7eZArlXy1PRKaXFlCwdt8d5uBVNkFYyUmP
 
 -- Dumped from database version 18.1
 -- Dumped by pg_dump version 18.1
@@ -29566,7 +29566,7 @@ COPY public.seasons_writers (ordering, show_id, season, writer_id) FROM stdin;
 --
 
 COPY public.users (id, is_admin, email, password, rating_scale) FROM stdin;
-1	t	austinrock03@gmail.com	$2a$12$AOV0G6DRHhHgUdKAJvNgE.L3vyBjM.6xfjO0rZQKCetMoJPfnEz5S	2
+1	t	austinrock03@gmail.com	$2a$12$AOV0G6DRHhHgUdKAJvNgE.L3vyBjM.6xfjO0rZQKCetMoJPfnEz5S	3
 \.
 
 
@@ -29830,5 +29830,5 @@ ALTER TABLE ONLY public.seasons_writers
 -- PostgreSQL database dump complete
 --
 
-\unrestrict RajgSL1e8uNAvifaKkgHVrzT8BpNdXQKEA1imKWGYLcbLdKA01rjOtv6F51BSC8
+\unrestrict J4thi70qUQOSTU1ldpS0pogcGaGnb7eZArlXy1PRKaXFlCwdt8d5uBVNkFYyUmP
 
