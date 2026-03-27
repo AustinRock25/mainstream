b0d0a03a074ad4b35326046ddd94ac4bc135ca5d
Austin-T-Rock <austinrock03@gmail.com>
2026-03-26 20:58:07 -0400

    Database backup: Thu 03/26/2026 20:57:50.37

--- api/src/routes/mediaRoutes.js
+++ api/src/routes/mediaRoutes.js
@@ -2,7 +2,7 @@ import { authorizeAdmin } from "../middleware/auth.js";
 import { index, indexLength, indexShows, seasonCount, indexNew, show, create, update } from "../controllers/mediaController.js";
 import { Router } from "express";
 const router = Router();
-import { validateMedia, validateMediaUpdate } from "../middleware/recordValidation.js";
+import { validateMedia } from "../middleware/recordValidation.js";
 
 router.get("/", index);
 router.get("/length", indexLength);
@@ -11,6 +11,6 @@ router.get("/seasons", seasonCount);
 router.get("/new", indexNew);
 router.get("/:id", show);
 router.post("/", [authorizeAdmin, validateMedia], create);
-router.put("/:id", [authorizeAdmin, validateMediaUpdate], update);
+router.put("/:id", [authorizeAdmin, validateMedia], update);
 
 export default router;
