8fe45c3afd65237bd04548c92625bbfccfa25b5c
Austin-T-Rock <austinrock03@gmail.com>
2026-05-13 10:43:16 -0400

    Database backup: Wed 05/13/2026 10:42:57.36

--- ui/src/components/modals/MediaForm.jsx
+++ ui/src/components/modals/MediaForm.jsx
@@ -520,7 +520,9 @@ function MediaForm({ show, setShow, media, season }) {
             <>
               <hr/>
                 <Form.Group as={Row} className="mb-3">
-                  <Form.Label column sm={3}>Grade: <span className="fw-light text-white-50">{getGrade(formData.grade)}</span></Form.Label>
+                  {user.rating_scale == 1 && <Form.Label column sm={3}>Grade: <span className={`fw-light text-${formData.grade <= 1.5 ? "danger" : formData.grade == 2 ? "warning" : "success"}`}>{getGrade(formData.grade)}</span></Form.Label>}
+                  {user.rating_scale == 1 && <Form.Label column sm={3}>Grade: <span className={`fw-light text-${formData.grade <= 1.5 ? "danger" : formData.grade <= 3 ? "warning" : "success"}`}>{getGrade(formData.grade)}</span></Form.Label>}
+                  {user.rating_scale == 1 && <Form.Label column sm={3}>Grade: <span className={`fw-light text-${formData.grade <= 3 ? "danger" : formData.grade <= 6 ? "warning" : "success"}`}>{getGrade(formData.grade)}</span></Form.Label>}
                   {user.rating_scale == 1 && <Col sm={9}><Form.Range min="0" max="4" step="0.5" value={formData.grade} onChange={(e) => handleChange(e, "grade")} /></Col>}
                   {user.rating_scale == 2 && <Col sm={9}><Form.Range min="0" max="5" step="0.5" value={formData.grade} onChange={(e) => handleChange(e, "grade")} /></Col>}
                   {user.rating_scale == 3 && <Col sm={9}><Form.Range min="0" max="12" step="1" value={formData.grade} onChange={(e) => handleChange(e, "grade")} /></Col>}
