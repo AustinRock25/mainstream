def7566c997cc541056a439818055520c063bdda
Austin-T-Rock <austinrock03@gmail.com>
2026-06-19 23:09:25 -0400

    Database backup: Fri 06/19/2026 23:09:05.73

--- ui/src/components/media/MediaModal.jsx
+++ ui/src/components/media/MediaModal.jsx
@@ -214,7 +214,7 @@ function MediaModal({ show, setShow, media, user, seasonCount }) {
                 <span className="fw-light fs-5 text-white-50">{getYear(media)}</span>
               </span>
             </div>
-            <div className="flex-column flex-md-row">
+            <div className="d-flex flex-column flex-md-row">
               <span className="fw-light fs-5 text-white-50">{media.rating === "Not Rated" ? "NR" : media.rating}</span>
               <span className="fw-light fs-5 text-white-50">{time(media.runtime || media.runtime_tv)}</span>
               {!user && <span className={`fw-bold fs-5 text-${(media.grade || media.grade_tv) <= 40 ? "danger" : (media.grade || media.grade_tv) <= 60 ? "warning" : "success"}`}>{getGrade(media)}</span>}
