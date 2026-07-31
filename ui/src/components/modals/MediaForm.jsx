611ec275cf60b4c3d64227fe0f22047bc5f7966e
Austin-T-Rock <austinrock03@gmail.com>
2026-07-31 08:52:37 -0400

    Database backup: Fri 07/31/2026  8:52:23.24

--- ui/src/components/modals/MediaForm.jsx
+++ ui/src/components/modals/MediaForm.jsx
@@ -393,7 +393,7 @@ function MediaForm({ show, setShow, media, season }) {
             <>
               <hr/>
                 <Form.Group as={Row} className="mb-3">
-                  <Form.Label column sm={3}>Grade: <span className={`fw-light text-${Math.round((Number.parseFloat((formData.grade * 100) / (user.rating_scale_max - user.rating_scale_min)) + Number.parseFloat(100)) / 2) <= 69 ? "danger" : Math.round((Number.parseFloat((formData.grade * 100) / (user.rating_scale_max - user.rating_scale_min)) + Number.parseFloat(100)) / 2) <= 79 ? "warning" : "success"}`}>{getGrade(formData.grade)}</span></Form.Label>
+                  <Form.Label column sm={3}>Grade: <span className={`fw-light text-${Math.round((formData.grade * 100) / (user.rating_scale_max - user.rating_scale_min)) <= 39 ? "danger" : Math.round((formData.grade * 100) / (user.rating_scale_max - user.rating_scale_min)) <= 60 ? "warning" : "success"}`}>{getGrade(formData.grade)}</span></Form.Label>
                   <Col sm={9}><Form.Range min="0" max={user.rating_scale_max - user.rating_scale_min} step="0.5" value={formData.grade} onChange={(e) => handleChange(e, "grade")} /></Col>
                   <Form.Control.Feedback type="invalid">{errors.grade}</Form.Control.Feedback>
                 </Form.Group>
